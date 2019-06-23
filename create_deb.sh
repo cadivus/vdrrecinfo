683de41b6075c19085d02b696875c0d9fbd28367
Jonas Greifenhain <jonas@greifenhain.eu>
2019-06-23 19:34:46 +0200

    new features, refactoring

--- create_deb.sh
+++ create_deb.sh
@@ -27,7 +27,6 @@ create_control "Security: base"
 create_control "Priority: optional"
 create_control "Architecture: all"
 create_control "Depends: python3"
-#create_control "Maintainer: JG <cadivus@daverkomp.de>"
 create_control "Description: Is for getting information about VDR-recordings"
 
 mkdir -p $deb_dir/usr/lib
